f38d19becbd773940f8e6fc354b1b47303d310e5
Chininpq123 <uv17903@gmail.com>
2023-10-28 15:51:30 -0600

    sumar dos numeros

---
+++ suma.java
@@ -0,0 +1,17 @@
+public class suma {
+    public class SumarDosNumeros {
+
+        public static void main(String[] args) {
+            // Declaramos las variables
+            int numero1 = 10;
+            int numero2 = 20;
+    
+            // Realizamos la suma
+            int suma = numero1 + numero2;
+    
+            // Imprimimos el resultado
+            System.out.println("La suma de los números " + numero1 + " y " + numero2 + " es " + suma);
+        }
+    }
+    
+}
